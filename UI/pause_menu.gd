01561d89a0b91dff7639ea19373439c955aae8cc
DankArmin <armangaspy@gmail.com>
2023-10-27 02:10:45 +0200

    add pause menu

---
+++ UI/pause_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@export var game_manager : GameManager
+
+func _ready():
+	hide()
+	game_manager.connect("on_game_paused", _on_game_manager_on_game_paused)
+
+
+func _on_game_manager_on_game_paused(is_paused : bool):
+	if is_paused:
+		show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		hide()
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_resume_button_pressed():
+	game_manager.is_game_paused = false
+
+
+func _on_save_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_load_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
