8cba2f4f34a4b87d056bf3a97531020cc33a16dc
DankArmin <armangaspy@gmail.com>
2023-11-09 02:39:55 +0100

    improved sway

--- Scripts/weapon_holder.gd
+++ Scripts/weapon_holder.gd
@@ -1,24 +1,31 @@
 extends Node3D
 
-var mouse_mov 
+var mouse_mov_x 
+var mouse_mov_y
 var sway_treshold = 5
-var sway_lerp = 10
+var sway_lerp = 5
 
-@export var sway_left : Vector3
-@export var sway_right : Vector3
-@export var sway_normal : Vector3
+@export var sway_range : float
 
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouse_mov = -event.relative.x
+		mouse_mov_x = -event.relative.x
+		mouse_mov_y = event.relative.y
 
 
 func _process(delta):
-	if mouse_mov != null:
-		if mouse_mov > sway_treshold:
-			rotation = rotation.lerp(sway_left, sway_lerp * delta)
-		elif mouse_mov < -sway_treshold:
-			rotation = rotation.lerp(sway_right, sway_lerp * delta)
+	if mouse_mov_x != null:
+		if mouse_mov_x > sway_treshold:
+			rotation.y = lerp_angle(rotation.y, -sway_range, sway_lerp * delta)
+		elif mouse_mov_x < -sway_treshold:
+			rotation.y = lerp_angle(rotation.y, sway_range, sway_lerp * delta)
 		else:
-			rotation = rotation.lerp(sway_normal, sway_lerp * delta)
+			rotation.y = lerp_angle(rotation.y, 0, sway_lerp * delta)
+	if mouse_mov_y != null:
+		if mouse_mov_y > sway_treshold:
+			rotation.x = lerp_angle(rotation.x, sway_range, sway_lerp * delta)
+		elif mouse_mov_y < -sway_treshold:
+			rotation.x = lerp_angle(rotation.x, -sway_range, sway_lerp * delta)
+		else:
+			rotation.x = lerp_angle(rotation.x, 0, sway_lerp * delta)
